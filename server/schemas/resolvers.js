04e9805dbc5d8c8ba377f4c37e509984955efee4
Faz131 <fazms@icloud.com>
2023-05-21 20:25:21 -0700

    added dependancies for react, graphql and apollo

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,54 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select(
+                    '-__v -password'
+                );
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in')
+        },
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Wrong Credentials');
+            }
+            const correctPass = await user.isCorrectPass(password);
+            if (!correctPass) {
+                throw new AuthenticationError('Wrong Credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        saveBook: async (parent, { input }, context) => {
+            if (context.user) {
+                const updateUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true, runValidators: true }
+                );
+                return updateUser;
+            }
+            throw new AuthenticationError('You must be logged in')
+
+        }
+    }
+}
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    savedBooks: [Book]
+    bookCount: Int
+   }
+   
+   type Book{
+    bookID: ID
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+}
+type Auth{
+    token: ID!
+user: User
+}
+
+input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+}
+
+type Query{
+    me: User
+}
+
+type Mutation{
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(input: String!): User
+}
+   }
+`;
+
+module.exports = typeDefs;
