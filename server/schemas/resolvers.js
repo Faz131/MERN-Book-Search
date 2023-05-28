224b4733ffcd099dfead85ad95384f898ddbadfa
Faz131 <fazms@icloud.com>
2023-05-28 16:39:21 -0700

    trying to troubleshoot connection between Atlas and Heroku

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -40,7 +40,7 @@ const resolvers = {
             if (context.user) {
                 const updateUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
-                    { $addToSet: { savedBooks: { bookId: bookId } } },
+                    { $addToSet: { savedBooks: input } },
                     { new: true, runValidators: true }
                 );
                 return updateUser;
--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.get(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 // Create a new instance of an Apollo server with the GraphQL schema
