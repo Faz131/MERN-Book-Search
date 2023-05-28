f1a310eca836fbba083cce3c302b9df5e11268a3
Faz131 <fazms@icloud.com>
2023-05-28 14:02:18 -0700

    updated config.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -9,7 +9,7 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
@@ -68,15 +68,15 @@ const SearchBooks = () => {
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+    // if (!token) {
+    //   return false;
+    // }
 
     try {
       // const response = 
-      await saveBook(bookToSave, token);
+      await saveBook(bookToSave);
 
       // if (!response.ok) {
       //   throw new Error('something went wrong!');
